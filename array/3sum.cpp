ddc2fcedd78c9bc2955f229c2e98aae6ccf4ff1b
Dev Jangra <devjangra822@gmail.com>
2025-12-29 21:08:10 +0530

    added Pascal triangle solution with nCr approach

---
+++ array/3sum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public :
+    vector<vector<int>>ThreeSum(vector<int>&nums){
+     sort(nums.begin(),nums.end());
+     set<vector<int>>s;
+     int left =0, right = nums.size()-1;
+     while(left < right){
+        if(nums[left]+nums[right] > 0)
+     }
+ }
+};
+int main(){
+    
+
+}
+
--- array/pascalTriangle.cpp
+++ array/pascalTriangle.cpp
@@ -1,32 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Pascal{
-    public:
-    vector<int>generateRow(int row){
-        long long ans = 1;
-        vector<int>ansRow;
+
+/*
+Problem: Pascal's Triangle
+Approach:
+- Each row is generated using nCr formula
+- nth row has n elements
+- Use previous value to compute next value efficiently
+
+Time Complexity: O(N^2)
+Space Complexity: O(N^2)
+*/
+
+class Pascal {
+public:
+    // Generates a single row of Pascal's Triangle
+    vector<int> generateRow(int row) {
+        long long value = 1;
+        vector<int> ansRow;
         ansRow.push_back(1);
-        for(int i = 1;i<row;i++){
-            ans = ans * (row-i);
-            ans /= i;
-            ansRow.push_back(ans);
+
+        for (int i = 1; i < row; i++) {
+            value = value * (row - i);
+            value = value / i;
+            ansRow.push_back(value);
         }
-         return ansRow;
+        return ansRow;
     }
-    vector<vector<int>>pasTriangle(int N){
-        vector<vector<int>>ans;
-        for(int i =1;i<=N;i++){
+
+    // Generates Pascal's Triangle up to N rows
+    vector<vector<int>> pascalTriangle(int N) {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= N; i++) {
             ans.push_back(generateRow(i));
         }
         return ans;
     }
 };
-int main(){
-  Pascal obj;
-  int n = 4;
-  vector<vector<int>>result = obj.pasTriangle(n);
-   for (auto &row : result) {
-        for (auto &val : row) cout << val << " ";
+
+int main() {
+    Pascal obj;
+    int n = 4;
+
+    vector<vector<int>> result = obj.pascalTriangle(n);
+
+    for (auto &row : result) {
+        for (auto &val : row) {
+            cout << val << " ";
+        }
         cout << endl;
     }
+
+    return 0;
 }
